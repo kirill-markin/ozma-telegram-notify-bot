9f46e7f4b6257b2a8382d084cb468bb82d920389
Kirmark <37423353+Kirmark@users.noreply.github.com>
2020-11-26 09:17:15 +0300

    config fix

--- quick_ubuntu_20_lts_startup.sh
+++ quick_ubuntu_20_lts_startup.sh
@@ -21,28 +21,28 @@ if [ "$1" != "-r" ]; then
   cp app/config_example.json app/config.json
 
   read -p 'token for the telegram bot: ' token
-  jq -c --arg a "$token" '.token = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$token" '.token = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
   read -p 'db_client_id for the application: ' db_client_id
-  jq -c --arg a "$db_client_id" '.db_client_id = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$db_client_id" '.db_client_id = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
   read -p 'db_client_secret for the application: ' db_client_secret
-  jq -c --arg a "$db_client_secret" '.db_client_secret = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$db_client_secret" '.db_client_secret = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
   read -p 'db_address - database api address: ' db_address
-  jq -c --arg a "$db_address" '.db_address = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$db_address" '.db_address = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
   read -p 'db_username - Username in ozma: ' db_username
-  jq -c --arg a "$db_username" '.db_username = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$db_username" '.db_username = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
   read -sp 'db_password - Password in ozma: ' db_password
-  jq -c --arg a "$db_password" '.db_password = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$db_password" '.db_password = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
   read -sp 'db_schema_name - Password in ozma: ' db_schema_name
-  jq -c --arg a "$db_schema_name" '.db_schema_name = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$db_schema_name" '.db_schema_name = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
   read -sp 'db_view_name - Password in ozma: ' db_view_name
-  jq -c --arg a "$db_view_name" '.db_view_name = $a' app/config.json > tmp.$$.json && mv tmp.$$.json config.json
+  jq -c --arg a "$db_view_name" '.db_view_name = $a' app/config.json > tmp.$$.json && mv tmp.$$.json app/config.json
 
 fi
 
